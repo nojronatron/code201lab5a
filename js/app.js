a3bbf34ca84ed3db9083035d85947f2f38f8ab00
Jonathan Rumsey <ricochetrider@hotmail.com>
2022-03-11 18:29:23 -0800

    implemented multiply any array feature function.

--- js/app.js
+++ js/app.js
@@ -143,10 +143,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  let elementProd = 1;
+  let prodResultNumberIdx = 0;
 
+  for (let idx = 0; idx < dynamicArray.length; idx++) {
+    elementProd = multiply(elementProd, dynamicArray[idx])[prodResultNumberIdx];
+  }
+
+  return [elementProd,
+    `The numbers ${dynamicArray} have a product of ${elementProd}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
